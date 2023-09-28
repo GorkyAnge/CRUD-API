697c67151c5cfb98d1ea485b4c860e2cd7be5fe3
Gorkyange <gorkyange@gmail.com>
2023-09-27 20:45:45 -0500

    Agregar archivos de proyecto.

---
+++ EjercicioEnClase2/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace EjercicioEnClase2.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ EjercicioEnClase2/Util/Utils.cs
@@ -0,0 +1,17 @@
+﻿using EjercicioEnClase2.Models;
+
+namespace EjercicioEnClase2.Util
+{
+    public class Utils
+    {
+        public static List<Producto> ListaProducto = new List<Producto>()
+        { new Producto()
+        {
+            Id = 1,
+            Nombre = "Producto1",
+            Descripcion = "Desc1",
+            Cantidad = 1
+        }
+        };
+    }
+}
